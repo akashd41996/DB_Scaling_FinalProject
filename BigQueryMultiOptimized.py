bae8544866b6abef4b189fc4eaff65c867c81008
akashd41996 <122998768+akashd41996@users.noreply.github.com>
2023-12-03 12:34:41 -0600

    Add files via upload

---
+++ BigQueryMultiOptimized.py
@@ -0,0 +1,139 @@
+import threading
+import time
+from google.cloud import bigquery
+
+# Initialize a BigQuery client
+client = bigquery.Client()
+
+# List of complex queries for benchmarking
+queries = [
+    """
+    SELECT PULocationID, COUNT(*) AS TotalTrips, AVG(trip_time) AS AverageTime
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    GROUP BY PULocationID
+    ORDER BY TotalTrips DESC
+
+    """,
+    """
+    SELECT *
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    WHERE (tolls > 5 AND trip_miles > 10) OR (congestion_surcharge > 2 AND trip_time > 1800)
+
+    """,
+    """
+    SELECT DATE(Pickup_datetime) AS PickupDate, COUNT(*) AS NumberOfTrips
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    WHERE EXTRACT(HOUR FROM Pickup_datetime) BETWEEN 8 AND 10
+    GROUP BY PickupDate
+    ORDER BY NumberOfTrips DESC
+
+    """,
+    """
+    SELECT Hvfhs_license_num, Pickup_datetime, RANK() OVER (PARTITION BY Hvfhs_license_num ORDER BY trip_time DESC) AS Rank
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    WHERE trip_time IS NOT NULL
+
+    """,
+    """
+    SELECT t.Hvfhs_license_num, t.Pickup_datetime, t.DropOff_datetime, avg_data.AverageTripTime
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table` t
+    JOIN (
+        SELECT Hvfhs_license_num, AVG(trip_time) AS AverageTripTime
+        FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+        GROUP BY Hvfhs_license_num
+    ) avg_data ON t.Hvfhs_license_num = avg_data.Hvfhs_license_num
+
+    """,
+    """
+     SELECT DISTINCT driver_pay
+     FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+     WHERE Hvfhs_license_num IN (
+         SELECT Hvfhs_license_num
+         FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+         WHERE trip_miles > 20
+ 
+     )
+ 
+     """,
+
+    """
+   SELECT SUBSTR(CAST(Pickup_datetime AS STRING), 1, 10) AS PickupDate, COUNT(*) AS NumTrips
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    GROUP BY PickupDate
+    ORDER BY NumTrips DESC
+
+    """,
+
+    """
+    SELECT Hvfhs_license_num, 
+           SUM(CASE WHEN trip_time > 1800 THEN 1 ELSE 0 END) AS LongTrips,
+           SUM(CASE WHEN trip_time <= 1800 THEN 1 ELSE 0 END) AS ShortTrips
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    GROUP BY Hvfhs_license_num
+
+    """,
+
+    # Query 9: JOIN with Multiple Conditions
+    """
+     WITH FilteredOrigin AS (
+    SELECT Hvfhs_license_num, Pickup_datetime, PULocationID
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    WHERE Pickup_datetime BETWEEN TIMESTAMP '2023-01-01 00:00:00' AND TIMESTAMP '2023-01-31 23:59:59'  -- Example timestamp range
+),
+FilteredDestination AS (
+    SELECT Hvfhs_license_num, DropOff_datetime, DOLocationID
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    WHERE DropOff_datetime BETWEEN TIMESTAMP '2023-01-01 00:00:00' AND TIMESTAMP '2023-01-31 23:59:59' -- Example timestamp range
+)
+SELECT a.Hvfhs_license_num, a.Pickup_datetime, b.DropOff_datetime
+FROM FilteredOrigin a
+JOIN FilteredDestination b ON a.Hvfhs_license_num = b.Hvfhs_license_num
+WHERE a.PULocationID != b.DOLocationID
+
+    """,
+    """
+    SELECT PULocationID, COUNT(*) AS TotalTrips, AVG(trip_time) AS AverageTripTime
+    FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table`
+    GROUP BY PULocationID
+    HAVING AVG(trip_time) > 600
+    """,
+    """
+        SELECT DISTINCT origin.Hvfhs_license_num
+        FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table` origin
+        WHERE origin.trip_miles > (
+            SELECT AVG(subquery.trip_miles)
+            FROM `dba-final-project-gcp.without_vm_upload.HV_Freq_Table` subquery
+        )
+        """
+]
+
+
+# Function to execute a query and measure the time taken
+def benchmark_query(query):
+    try:
+        start_time = time.time()
+        # Create a QueryJobConfig object with use_query_cache set to False
+        job_config = bigquery.QueryJobConfig(use_query_cache=False)
+
+        query_job = client.query(query, job_config=job_config)  # Run the query
+        results = query_job.result()  # Wait for the query to finish
+        end_time = time.time()
+
+        # Fetch the billing details from the completed query job
+        total_bytes_processed = query_job.total_bytes_processed
+        total_bytes_billed = query_job.total_bytes_billed or 0  # Use zero if None to handle free queries
+        cost_estimate = (total_bytes_billed / 1e12) * 5  # Assumes the cost of $5 per TB as per current pricing
+
+        # Print the execution details including the cost estimate
+        print(f"Query: {query}\nTime taken: {end_time - start_time} seconds")
+        print(f"Data processed (bytes): {total_bytes_processed}")
+        print(f"Data billed (bytes): {total_bytes_billed}")
+        print(f"Estimated cost (USD): {cost_estimate:.2f}\n")
+
+    except Exception as e:
+        print(f"Error: {e}\nFailed to execute query: {query}\n")
+
+
+# Run the benchmark on each query
+for query in queries:
+    benchmark_query(query)
---
+++ PostgresMultiThreading.py
@@ -0,0 +1,108 @@
+import time
+import psycopg2
+
+# PostgreSQL connection parameters
+host = "35.196.84.94"
+dbname = "database"
+user = "admin"
+password = "password"
+
+# List of complex queries for benchmarking
+queries = [
+    """
+    SELECT pulocationid, COUNT(*) AS totaltrips, AVG(trip_time) AS averagetime
+    FROM hv_vehicle_trip
+    GROUP BY pulocationid
+    ORDER BY totaltrips DESC
+    """,
+    """
+    SELECT *
+    FROM hv_vehicle_trip
+    WHERE (tolls > 5 AND trip_miles > 10) OR (congestion_surcharge > 2 AND trip_time > 1800)
+    """,
+    """
+    SELECT SUBSTRING(pickup_datetime, 12, 2) AS PickupHour, COUNT(*) AS NumberOfTrips
+    FROM hv_vehicle_trip
+    WHERE SUBSTRING(pickup_datetime, 12, 2) BETWEEN '08' AND '10'
+    GROUP BY PickupHour
+    ORDER BY NumberOfTrips DESC
+
+
+    """,
+    """
+    SELECT hvfhs_license_num, pickup_datetime, RANK() OVER (PARTITION BY hvfhs_license_num ORDER BY trip_time DESC) AS rank
+    FROM hv_vehicle_trip
+    WHERE trip_time IS NOT NULL
+    """,
+    """
+    SELECT t.hvfhs_license_num, t.pickup_datetime, t.dropoff_datetime, avg_data.averagetriptime
+    FROM hv_vehicle_trip t
+    JOIN (
+        SELECT hvfhs_license_num, AVG(trip_time) AS averagetriptime
+        FROM hv_vehicle_trip
+        GROUP BY hvfhs_license_num
+    ) avg_data ON t.hvfhs_license_num = avg_data.hvfhs_license_num
+    """,
+
+    """
+    SELECT DISTINCT driver_pay
+FROM hv_vehicle_trip
+WHERE hvfhs_license_num IN (
+    SELECT hvfhs_license_num
+    FROM hv_vehicle_trip
+    WHERE trip_miles > 20
+)
+
+    """,
+    """
+   SELECT SUBSTRING(pickup_datetime FROM 1 FOR 10) AS PickupDate, COUNT(*) AS NumTrips
+FROM hv_vehicle_trip
+GROUP BY PickupDate
+ORDER BY NumTrips DESC
+
+
+   """,
+    """
+   SELECT hvfhs_license_num, 
+         SUM(CASE WHEN trip_time > 1800 THEN 1 ELSE 0 END) AS LongTrips,
+         SUM(CASE WHEN trip_time <= 1800 THEN 1 ELSE 0 END) AS ShortTrips
+  FROM hv_vehicle_trip
+  GROUP BY hvfhs_license_num
+  
+    """,
+    """
+    SELECT a.hvfhs_license_num, a.pickup_datetime, b.dropoff_datetime
+  FROM hv_vehicle_trip a
+  JOIN hv_vehicle_trip b ON a.hvfhs_license_num = b.hvfhs_license_num AND a.pulocationid != b.dolocationid
+  LIMIT 10000
+  
+    """,
+    """
+   SELECT pulocationid, COUNT(*) AS TotalTrips, AVG(trip_time) AS AverageTripTime
+  FROM hv_vehicle_trip
+  GROUP BY pulocationid
+  HAVING AVG(trip_time) > 600
+  
+  """
+]
+
+
+# Function to execute a query and measure the time taken
+def benchmark_query(query, conn):
+    start_time = time.time()
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+    end_time = time.time()
+    print(f"Query:\n{query}\nTime taken: {end_time - start_time} seconds\n")
+
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+
+# Run the benchmark on each query
+try:
+    for query in queries:
+        benchmark_query(query, conn)
+finally:
+    conn.close()
